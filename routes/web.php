930f04de6b8dc8731c1362a491a08161a9155ce6
Wildan <wildanwigenta52@gmail.com>
2025-10-15 15:01:50 +0700

    add route web

--- routes/web.php
+++ routes/web.php
@@ -1,7 +1,14 @@
 <?php
 
-use Illuminate\Support\Facades\Route;
 
+use Illuminate\Support\Facades\Route;
+use App\Http\Controllers\HomeController;
+use App\Http\Controllers\ProductController;
+
+Route::get('/', [HomeController::class, 'index'])->name('home');
+Route::get('/products', [ProductController::class, 'index'])->name('products.index');
+Route::get('/products/{slug}', [ProductController::class, 'show'])->name('products.show');
+Route::get('/catalogue', [ProductController::class, 'catalogue'])->name('catalogue');
 Route::get('/', function () {
     return view('welcome');
 });
