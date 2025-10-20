85ce79d57654c2d2c5446ff35e107abd2d0d75da
Wildan <wildanwigenta52@gmail.com>
2025-10-20 10:55:17 +0700

    add favicon logo

--- app/Providers/Filament/AdminPanelProvider.php
+++ app/Providers/Filament/AdminPanelProvider.php
@@ -26,6 +26,7 @@ class AdminPanelProvider extends PanelProvider
             ->default()
             ->id('admin')
             ->path('admin')
+            ->favicon(asset('images/logo_c.png'))
             ->login()
             ->colors([
                 'primary' => Color::Amber,
