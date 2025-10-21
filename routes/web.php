8018fc106a2f24b17b0e85f5deadb09a66e05691
Wildan <wildanwigenta52@gmail.com>
2025-10-21 19:25:27 +0700

    update route

--- routes/web.php
+++ routes/web.php
@@ -8,4 +8,3 @@ Route::get('/', [HomeController::class, 'index'])->name('home');
 Route::get('/products', [ProductController::class, 'index'])->name('products.index');
 Route::get('/products/{slug}', [ProductController::class, 'show'])->name('products.show');
 Route::get('/catalogue', [ProductController::class, 'catalogue'])->name('catalogue');
-
